3db000adc2c7b5eefc7c5511f374c056467bc82f
TsarArt <63518665+TsarArt@users.noreply.github.com>
2024-03-20 00:29:50 +0200

    Complete

--- 142/script/main.js
+++ 142/script/main.js
@@ -1,5 +1,34 @@
 document
     .querySelector('#previous')
     .addEventListener('click', function() {
-        
+        let colorId = getColorId(document.querySelectorAll('#imgs li img')),
+            colors = document.querySelectorAll('#imgs li img'),
+            imgList = ['wood.png', 'glass.png', 'linoleum.png', 'cloth.png', 'marble.png'],
+            mainImg = document.querySelector('#active-texture');
+
+        colors[colorId].classList.remove('active');
+        colorId--;
+        if (colorId < 0) colorId = colors.length - 1;
+        colors[colorId].classList.add('active');
+        mainImg.src = `img/${imgList[colorId]}`;
     });
+
+document
+    .querySelector('#next')
+    .addEventListener('click', function() {
+        let colorId = getColorId(document.querySelectorAll('#imgs li img')),
+            colors = document.querySelectorAll('#imgs li img'),
+            imgList = ['wood.png', 'glass.png', 'linoleum.png', 'cloth.png', 'marble.png'],
+            mainImg = document.querySelector('#active-texture');
+
+        colors[colorId].classList.remove('active');
+        colorId = ++colorId % colors.length;
+        colors[colorId].classList.add('active');
+        mainImg.src = `img/${imgList[colorId]}`;
+    });
+
+function getColorId(colors) {
+    for (let i = 0; i < colors.length; i++) {
+        if (colors[i].classList.contains('active')) return i;
+    }
+}
